fff7cc37116cbd526bece0b2484a44a647ca30e1
air2021-dev <209369611+air2021-dev@users.noreply.github.com>
2025-06-05 23:06:39 +0900

    feat(web): build server
    - wiki_handler(Path(slug): Path<String>) -> Html<String>
     - ferriki-core 모듈에서 문서를 읽어오는 함수
    
    - change fn main -> async fn main
     - tokio 매크로 추가
     - 서버를 실행 할 수 있도록 서버 생성 로직 추가
     - route 추가

--- ferriki-web/src/main.rs
+++ ferriki-web/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{extract::Path, response::Html, routing::get, Router};
+use ferriki_core::load_markdown;
+use pulldown_cmark::{Parser, html::push_html};
+use tokio::net::TcpListener;
+async fn wiki_handler(Path(slug): Path<String>) -> Html<String> {
+    match load_markdown(&slug){
+        Ok(md) => {
+            let parser = Parser::new(&md);
+            let mut html_output = String::new();
+            push_html(&mut html_output, parser);
+
+            Html(html_output)
+        },
+        Err(msg) => {
+            Html(format!("<h1>404 Not Found</h1><p>{}</p>", msg))
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, Ferriki!"}))
+        .route("/wiki/{slug}", get(wiki_handler));
+
+    let addr = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+
+    axum::serve(addr, app).await.unwrap();
 }
