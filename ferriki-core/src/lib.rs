5be9f10bd9dfbcd292c2e510cd01e098117f5d1d
air2021-dev <209369611+air2021-dev@users.noreply.github.com>
2025-06-05 23:04:15 +0900

    feat(core): modify load_markdown
    - change return type Option<String> -> Result<String, String>
     - 문서 읽어 올때, 해당 문서가 존재하지 않으면 에러 메시지를 리턴할 수 있도록 수정
     - 문서가 존재하면 문서를 읽어서 해당 내용을 리턴

--- ferriki-core/src/lib.rs
+++ ferriki-core/src/lib.rs
@@ -1,9 +1,10 @@
 use std::fs;
 use std::path::PathBuf;
 
-pub fn load_markdown(slug: &str) -> Option<String> {
+// 마크다운 문서를 읽어옴
+pub fn load_markdown(slug: &str) -> Result<String, String> {
     let mut path = PathBuf::from("docs");
     path.push(format!("{}.md", slug));
-    
-    fs::read_to_string(path).ok()
+
+    fs::read_to_string(&path).map_err(|_| format!("문서 '{}'를 찾을 수 없습니다.", slug))
 }
